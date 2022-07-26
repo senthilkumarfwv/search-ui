54ca5ce92f5751dd9c5dfa2929bf221aa761077f
senthilkumarfwv <senthilkumar.fwv@gmail.com>
2022-07-26 19:40:05 +0530

    Auto-generated commit [ci skip]

--- src/app/modules/shared/common.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CommonDirective } from './common.directive';
-
-describe('CommonDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CommonDirective();
-    expect(directive).toBeTruthy();
-  });
-});
